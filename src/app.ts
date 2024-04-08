80d068c15916c3dcd56db24b6551db2b7ef564f4
Sid Patel <sid.auburn@gmail.com>
2024-04-08 17:36:09 -0400

    setting the local mongo container

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { Express } from "express";
 import { backendServer } from "./setupServer";
+import databaseConnection from "./setupDatabase";
 
 class Application {
   public initialize(): void {
+    databaseConnection();
     const app: Express = express();
     const server = new backendServer(app);
     server.start();
--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -3,13 +3,12 @@ import mongoose from "mongoose";
 export default () => {
   const connnect = async () => {
     try {
-      await mongoose.connect("mongodb://localhost:27017/express-typescript", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
-      console.log("Database connected");
+      await mongoose.connect("mongodb://root:password@localhost:27017/");
+      console.log("Database connected !!");
     } catch (error) {
       console.log("Database connection error", error);
     }
   };
+  connnect();
+  mongoose.connection.on("disconnected", connnect);
 };
